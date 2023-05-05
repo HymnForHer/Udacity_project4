9f673f4b468879e95aed8ee414613730359cc764
Hymn <danh208pt@gmail.com>
2023-05-05 21:17:23 +0700

    Update until task 7

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath="hymn208/udacity-project4:v1.0.0"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-project4 \
+        --image=hymn208/udacity-project4:v1.0.0 --port=80 \
+        --labels app=udacity-project4
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pod
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-project4 8000:80
